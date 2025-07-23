264187e097eb9cdd97fb816242d0117bbe01baf0
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-24 00:01:18 +0530

    new codes

--- AgeCondition.java
+++ AgeCondition.java
@@ -1,10 +1,17 @@
 import java.util.*;
 public class AgeCondition {
+    // check the age of the person using if else
+    //1st code
+    
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
         if (age >= 18) {
             System.out.println("person is an aduld");
+        }
+
+        if (age>13 && age<=18) {
+            System.out.println("teenager");
         }
         else {
             System.out.println("Person is not an adult");
---
+++ largestno.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class largestno {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        System.out.println("type A : " + A);
+        int B = sc.nextInt();
+         System.out.println("type B : " + B);
+       
+
+        if (A>B) {
+            System.out.println(A + "is greater");
+        }
+        else {
+            System.out.println(B+ "is greater");
+        }
+
+
+
+    }
+    
+}
