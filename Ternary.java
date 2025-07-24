0b17c0c8c81eeaaa5cc36dfeaae64db3aadf0c6c
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-24 19:11:09 +0530

    Add a new code

---
+++ Ternary.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Ternary {
+    public static void main(String args[]){
+    //     int number = 4;
+    //     //using ternary operator
+    //   String type = ( (number % 2) == 0) ? "even" : "odd" ;
+    //   System.out.println(type);
+
+
+        int larger = (5>6) ? 5:6;
+        System.out.println(larger);
+
+
+    }
+}
