149e91ae69556589d0ef12b7121317abd4abc40b
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-24 18:54:18 +0530

    Add a new code

---
+++ largestofth.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class largestofth {
+    public static void main (String args[]){
+        int A = 16;
+        int B = 285;
+        int C = 33;
+
+        if (A>=B && A>=C){
+            System.out.println("A");
+        }
+        else if (B>=C){
+            System.out.println("B");
+        }
+        else {
+            System.out.println("C");
+        }
+    }
+    
+}
