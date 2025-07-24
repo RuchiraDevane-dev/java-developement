807671ee45e6d840dc71699c91346f3f51cd2a44
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-24 22:38:56 +0530

    Add a new code

---
+++ temp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class temp {
+    public static void main (String args[]) {
+        double temp = 103.5;
+        if(temp>100) {
+            System.out.print("You have a fever");
+        }
+        else {
+            System.out.println("You don't have a fever");
+        }
+    }
+    
+}
