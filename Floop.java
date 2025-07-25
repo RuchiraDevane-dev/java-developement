64180557d8d8ac0bafa2151a9dff4def1b8b2cb0
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-26 00:24:21 +0530

    add a code

---
+++ Floop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Floop {
+    public static void main(String args[]){
+        int i;
+        for(i=1; i<=5; i++) {
+            System.out.println("Hello Ruchi");
+        }
+    }
+    
+}
