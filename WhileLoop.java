49fa99e7eb0b665ceef799655f9d6117794592c6
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-25 14:56:41 +0530

    add a code

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class WhileLoop {
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter < 5) {
+            System.out.println("Hello World!");
+            counter ++;
+        }
+    }
+}
