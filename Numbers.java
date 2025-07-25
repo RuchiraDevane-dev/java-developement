f71c192f19c1eac87befcae16e2e84a080850ea8
Ruchira Devane <Ruchiradevane24@gmail.com>
2025-07-25 22:22:22 +0530

    add a code

---
+++ Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Numbers {
+    public static void main(String args[]) {
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.print(counter);
+            counter ++;
+        }
+    }
+}
